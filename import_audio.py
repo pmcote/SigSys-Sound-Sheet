127610ee1ac1566b7aeb7b59c74f4c732d780138
Sophia Seitz <phia.seitz@gmail.com>
2014-04-14 14:21:00 -0400

    Found some code online for graphing a sound file. I'll go through and comment the code now.

---
+++ import_audio.py
@@ -0,0 +1,26 @@
+## Found code from: http://stackoverflow.com/questions/18625085/how-to-plot-a-wav-file
+import matplotlib.pyplot as plt
+import numpy as np
+import wave
+import sys
+
+
+spf = wave.open('beep-01a.wav','r')
+
+#Extract Raw Audio from Wav File
+signal = spf.readframes(-1)
+signal = np.fromstring(signal, 'Int16')
+fs = spf.getframerate()
+
+#If Stereo
+if spf.getnchannels() == 2:
+    print 'Just mono files'
+    sys.exit(0)
+
+
+Time=np.linspace(0, len(signal)/fs, num=len(signal))
+
+plt.figure(1)
+plt.title('Signal Wave...')
+plt.plot(Time,signal)
+plt.show()
